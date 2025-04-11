74ad16bff395905344d4b75708e1b2fc3e7b40ae
Vidhi Subudhi <143302633+vidhi6040@users.noreply.github.com>
2025-04-11 16:24:38 +0530

    Update objective_two.py

--- objective_two.py
+++ objective_two.py
@@ -16,6 +16,7 @@ df = df[df["Location"] != "All India"]
 gender_gap = df.groupby(["Location", "School Type"])["Total No. of Schools"].sum().unstack(fill_value=0)
 gender_gap
 
+#A. Boys
 plt.figure(figsize=(18, 8))
 plt.plot(gender_gap.index, gender_gap["Boys"], label="Boys Schools", color="#4058ef")
 plt.xticks(rotation=90)
@@ -23,6 +24,7 @@ plt.legend()
 plt.ylabel("No. Of Schools")
 plt.title("Boys Schools Across India")
 
+#B. Girls
 plt.figure(figsize=(18, 8))
 plt.plot(gender_gap.index, gender_gap["Girls"], label="Girls Schools", color="#f577e4")
 plt.xticks(rotation=90)
@@ -30,6 +32,7 @@ plt.legend()
 plt.ylabel("No. Of Schools")
 plt.title("Girls Schools Across India")
 
+#C. Co-ed
 plt.figure(figsize=(18, 8))
 plt.plot(gender_gap.index, gender_gap["Co-Ed"], label="Co-Educational Schools", color="#34a154")
 plt.xticks(rotation=90)
@@ -37,6 +40,7 @@ plt.legend()
 plt.ylabel("No. Of Schools")
 plt.title("Co-ed Schools Across India")
 
+#D. Girls Vs Boys Vs Co-ed
 plt.figure(figsize=(18, 8))
 plt.plot(gender_gap.index, gender_gap["Boys"], label="Boys Schools", color="#4058ef")
 plt.plot(gender_gap.index, gender_gap["Girls"], label="Girls Schools", color="#f577e4")

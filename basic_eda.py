e0a3906ccab2552a808935dadb31f1c510d20dc5
Vidhi Subudhi <143302633+vidhi6040@users.noreply.github.com>
2025-04-12 00:08:08 +0530

    Update basic_eda.py

--- basic_eda.py
+++ basic_eda.py
@@ -4,13 +4,20 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
+#Reading data from csv file
 df = pd.read_csv("C:\\Users\\mailt\\Desktop\\Diya\\DS_py\\Project\\Dataset.csv")
 
 
+# Clean column names
+df.columns = df.columns.str.strip().str.replace("_", "  ").str.replace("(", "").str.replace(")", "").str.replace(",", "").str.replace("’", "").str.replace("'", "").str.replace(".", "")
+
+
+#DataSet
 print("Dataset Overview: ")
 print(df)
 
 
+#Basic EDA Prints
 print("Basic EDA prints: ")
 print(df.describe())
 print(df.info())
@@ -19,3 +26,31 @@ print(df.tail())
 print(df.columns)
 print(df.shape)
 print(df.isnull().sum())
+
+
+# Map school managements
+management_map = {
+    # Private
+    "Private Unaided (Recognized)": "Private",
+    "Unrecognized": "Private",
+    "Madarsa recognized (by Wakf board/Madarsa Board)": "Private",
+    "Madarsa unrecognized": "Private",
+    
+    # Government
+    "Department of Education": "Government",
+    "Government Aided": "Government",
+    "Tribal Welfare Department": "Government",
+    "Local body": "Government",
+    "Social welfare Department": "Government",
+    "Other Govt. managed schools": "Government",
+    "Kendriya Vidyalaya / Central School": "Government",
+    "Jawahar Navodaya Vidyalaya": "Government",
+    "Other Central Govt. Schools": "Government",
+    "Railway School": "Government",
+    "Sainik School": "Government",
+    "Ministry of Labor": "Government",
+    "Central Tibetan School": "Government"
+}
+df["School_Type_Grouped"] = df["School Management"].map(management_map)
+
+
